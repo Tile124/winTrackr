070b81ed0c8d0b0c61907fbb5468973332f6bd19
Ryan Rodriguez <tile124@icloud.com>
2023-03-29 22:01:51 -0400

    Added Angular materials

--- Frontend/winTrackr/src/app/app.module.ts
+++ Frontend/winTrackr/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StepperComponent } from './stepper/stepper.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { StepperComponent } from './stepper/stepper.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
